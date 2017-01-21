b8bbbb86ddfc6e71df1f44d94db0067598d66435
GamesPower <games.ultrapower@gmail.com>
2017-01-21 03:14:13 +0100

    Implement command sender and adding manager

--- MineLW/src/net/sociuris/minelw/server/MinecraftServer.java
+++ MineLW/src/net/sociuris/minelw/server/MinecraftServer.java
@@ -3,16 +3,21 @@ package net.sociuris.minelw.server;
 import java.io.File;
 
 import net.sociuris.logger.Logger;
+import net.sociuris.minelw.block.BlockManager;
+import net.sociuris.minelw.command.CommandManager;
+import net.sociuris.minelw.command.CommandSender;
+import net.sociuris.minelw.text.TextComponentArray;
+import net.sociuris.minelw.translation.MinecraftTranslation;
 import net.sociuris.util.StringUtils;
 
-public class MinecraftServer {
+public class MinecraftServer implements CommandSender {
 
 	private static MinecraftServer instance;
 
 	/**
-	 * Obtient l'instance de {@link MinecraftServer}
+	 * Gets the server instance
 	 * 
-	 * @return the {@link MinecraftServer} instance
+	 * @return the server instance
 	 */
 	public static MinecraftServer getInstance() {
 		return MinecraftServer.instance;
@@ -24,6 +29,9 @@ public class MinecraftServer {
 	private final Version version = new Version("1.11.2", new int[] { 0 });
 	private final File workingDirectory;
 	private final ServerProperties properties;
+
+	private final BlockManager blockRegistry = new BlockManager();
+	private final CommandManager commandManager = new CommandManager(this);
 
 	private boolean isStarted = true;
 
@@ -45,7 +53,7 @@ public class MinecraftServer {
 	public Version getVersion() {
 		return version;
 	}
-	
+
 	public File getWorkingDirectory() {
 		return workingDirectory;
 	}
@@ -56,6 +64,62 @@ public class MinecraftServer {
 
 	public boolean isStarted() {
 		return isStarted;
+	}
+
+	/**
+	 * Gets the server's "username"
+	 * 
+	 * @return the version name
+	 */
+	@Override
+	public String getUsername() {
+		return version.getVersionName();
+	}
+
+	/**
+	 * Checks if the server has given permission
+	 * 
+	 * @param permissionKey
+	 * @return {@code true} everytime
+	 */
+	@Override
+	public boolean hasPermission(String permissionKey) {
+		return true;
+	}
+
+	@Override
+	public void sendMessage(TextComponentArray textComponentArray) {
+		logger.info(textComponentArray.toPrintableMessage());
+	}
+
+	@Override
+	public void sendMessage(String message) {
+		logger.info(message);
+	}
+
+	@Override
+	public void translateAndSendMessage(String key, String... args) {
+		throw new UnsupportedOperationException("try to translate this: " + key);
+	}
+
+	@Override
+	public void sendMinecraftMessage(MinecraftTranslation minecraftTranslation, String... args) {
+		throw new UnsupportedOperationException("try to translate this: " + minecraftTranslation.getTranslationKey());
+	}
+
+	@Override
+	public void sendError(TextComponentArray error) {
+		logger.error(error.toPrintableMessage());
+	}
+
+	@Override
+	public void sendSuccess(TextComponentArray success) {
+		logger.success(success.toPrintableMessage());
+	}
+
+	@Override
+	public void sendWarning(TextComponentArray warning) {
+		logger.warn(warning.toPrintableMessage());
 	}
 
 }
