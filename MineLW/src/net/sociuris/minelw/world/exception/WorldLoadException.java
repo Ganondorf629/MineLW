4e25e7ae4c98a0bc0da2dc6dd0ff091cf28dccf4
GamesPower <games.ultrapower@gmail.com>
2017-01-22 00:03:28 +0100

    Add world exception

---
+++ MineLW/src/net/sociuris/minelw/world/exception/WorldLoadException.java
@@ -0,0 +1,14 @@
+package net.sociuris.minelw.world.exception;
+
+public class WorldLoadException extends WorldException {
+
+	private static final long serialVersionUID = 1L;
+
+	public WorldLoadException() {
+	}
+
+	public WorldLoadException(String message) {
+		super(message);
+	}
+
+}
