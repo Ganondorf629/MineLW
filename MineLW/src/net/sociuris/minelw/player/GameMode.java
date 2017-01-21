4c33185d5ea11da7cd0085f7679a6ab5a6a29da5
GamesPower <games.ultrapower@gmail.com>
2017-01-22 00:01:50 +0100

    Add getGameMode method for get it from id

--- MineLW/src/net/sociuris/minelw/player/GameMode.java
+++ MineLW/src/net/sociuris/minelw/player/GameMode.java
@@ -3,5 +3,13 @@ package net.sociuris.minelw.player;
 public enum GameMode {
 
 	SURVIVAL, CREATIVE, ADVENTURE, SPECTATOR;
+	
+	public static GameMode getGameMode(int id) {
+		GameMode[] gameModes = GameMode.values();
+		if(id < gameModes.length)
+			return gameModes[id];
+		else
+			return null;
+	}
 
 }
