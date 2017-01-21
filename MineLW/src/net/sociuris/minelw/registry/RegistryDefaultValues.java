c5772796e2af33d99af39f836e3b9c12710ff4f4
GamesPower <games.ultrapower@gmail.com>
2017-01-21 03:12:11 +0100

    Remove registry classes

--- MineLW/src/net/sociuris/minelw/registry/RegistryDefaultValues.java
+++
@@ -1,7 +0,0 @@
-package net.sociuris.minelw.registry;
-
-public interface RegistryDefaultValues {
-
-	public void registerDefault();
-	
-}
--- MineLW/src/net/sociuris/minelw/registry/RegistryNamespaced.java
+++
@@ -1,4 +0,0 @@
-package net.sociuris.minelw.registry;
-
-public abstract class RegistryNamespaced<V> extends Registry<String,V> {
-}
